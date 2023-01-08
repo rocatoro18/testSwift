fa363a304497ec202aec30a81e7ddbddef188cad
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-01-08 13:28:07 -0700

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Carlos Torres on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
